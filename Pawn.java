8bce5938563eef0f63cfb8df87255c2085a70393
Chuck Rozhon <rozhon2@illinois.edu>
2013-11-09 15:13:48 -0600

    Added assets files.

--- Pawn.java
+++ Pawn.java
@@ -8,7 +8,7 @@ public class Pawn extends Piece
 	
 	public Pawn(int x, int y, int width, int height)
 	{
-		super("WhitePawn.png", x, y, width, height);
+		super("assets/WhitePawn.png", x, y, width, height);
 	}
 	
 }
